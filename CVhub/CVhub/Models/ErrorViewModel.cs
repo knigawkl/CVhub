7ac59da3aeb1d03fdd00c15076b0edcd8fa71603
knigawkl <lukasz.knigawka@outlook.com>
2018-12-05 19:48:55 +0100

    Creating project structure

---
+++ CVhub/CVhub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CVhub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
