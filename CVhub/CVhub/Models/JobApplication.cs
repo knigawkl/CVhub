067813daa9510851c35015c13b08f5fd42603b5a
knigawkl <lukasz.knigawka@outlook.com>
2018-12-29 20:17:47 +0100

    Adding hidden input (it works now)git add .!

--- CVhub/CVhub/Models/JobApplication.cs
+++ CVhub/CVhub/Models/JobApplication.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -9,6 +10,8 @@ namespace CVhub.Models
     public class JobApplication
     {
         public int Id { get; set; }
+
+        [HiddenInput(DisplayValue = false)]
         public int OfferId { get; set; }
 
         [Required(ErrorMessage = "First Name required")]
